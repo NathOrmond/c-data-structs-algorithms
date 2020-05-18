a7859b30b0aac6ad60646120e8b39505ba52f024
NathOrmond <nath.m.ormond@gmail.com>
2020-05-18 17:35:17 +0100

    hex and binary printing

--- ch_1/ex_11.c
+++ ch_1/ex_11.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+unsigned int int_to_int(unsigned int k);
+
 int main(void){
+    printf("Enter a positive Integer:\n");
+    unsigned int input;
+    scanf("%d",&input);
+    
+    // Hexadecimal
+    printf("HEXADECIMAL: 0x%x\n", input);
+    
+    // Octal
+    
+
+    // Quarternary
+    
+
+    // Binary
+    printf("\nBINARY: %d\n", int_to_int(input));
 
     return 0;
+}
+
+unsigned int int_to_int(unsigned int k) {
+    return (k == 0 || k == 1 ? k : ((k % 2) + 10 * int_to_int(k / 2)));
 }
